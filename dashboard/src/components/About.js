9b31322d2afbac2dfb2d423779c08787b3a473e1
Jayman-Patel2001 <71970156+Jayman-Patel2001@users.noreply.github.com>
2023-04-25 13:18:02 +0530

    Update About.js
    Will create this page later on....

--- dashboard/src/components/About.js
+++ dashboard/src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Layout } from "antd";
 import logo from "../assests/aboutImg.png";
-import "../styles/About.css";
+// import "../styles/About.css";
 import { NavLink } from "react-router-dom";
 const { Content } = Layout;
 
@@ -19,46 +19,46 @@ const About = () => {
           color: "white",
         }}
       >
-        <div className="heading">
-          <h2>
-            We are
-            <NavLink className="electronLink" to="/">
-              Electron
-            </NavLink>
-          </h2>
-          <h3>Providing you with the real-time data analysis</h3>
-        </div>
-        <div className="aboutContent">
-          <img className="about-image" src={logo} alt="about" />
-          <div className="text-box">
-            <p>
-              At Electron, we want to provide you with data visualization:
-              <strong> Our data never sleeps</strong>.
-            </p>
-            <p>&nbsp;</p>
-            <p>
-              Companies want to keep the track of their data, performance of
-              their machines. Electron solves this problem by providing them
-              real time dashboard, from which they don't have to worry about
-              their data loss. From analysis to downtime, we check overall
-              performance of the machines and their connectivity.
-            </p>
-            <p>&nbsp;</p>
-            <p>
-              In corporate sector, we need <strong>data </strong>to make
-              accurate decisions. Electron provides a way to arrange this data
-              in a proper heirarchy. We are working under the guidance of
-              <a
-                className="nutronLink"
-                target="_blank"
-                href="https://nutronsystems.com"
-              >
-                Nutron
-              </a>
-              <span>.</span>
-            </p>
-          </div>
-        </div>
+//         <div className="heading">
+//           <h2>
+//             We are
+//             <NavLink className="electronLink" to="/">
+//               Electron
+//             </NavLink>
+//           </h2>
+//           <h3>Providing you with the real-time data analysis</h3>
+//         </div>
+//         <div className="aboutContent">
+//           <img className="about-image" src={logo} alt="about" />
+//           <div className="text-box">
+//             <p>
+//               At Electron, we want to provide you with data visualization:
+//               <strong> Our data never sleeps</strong>.
+//             </p>
+//             <p>&nbsp;</p>
+//             <p>
+//               Companies want to keep the track of their data, performance of
+//               their machines. Electron solves this problem by providing them
+//               real time dashboard, from which they don't have to worry about
+//               their data loss. From analysis to downtime, we check overall
+//               performance of the machines and their connectivity.
+//             </p>
+//             <p>&nbsp;</p>
+//             <p>
+//               In corporate sector, we need <strong>data </strong>to make
+//               accurate decisions. Electron provides a way to arrange this data
+//               in a proper heirarchy. We are working under the guidance of
+//               <a
+//                 className="nutronLink"
+//                 target="_blank"
+//                 href="https://nutronsystems.com"
+//               >
+//                 Nutron
+//               </a>
+//               <span>.</span>
+//             </p>
+//           </div>
+//         </div>
       </Content>
     </Layout>
   );
