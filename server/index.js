89d8258adc1c7f2cf44dc9fdc35a11c9513465c6
Jayman-Patel2001 <71970156+Jayman-Patel2001@users.noreply.github.com>
2023-04-25 13:14:50 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,79 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.text());
 
 let data = [];
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to client");
+  //! Call on machine metadata.......................................................................................
+  app.post("/api/machine/metadata", async (req, res) => {
+    data = eval(req.body);
+    try {
+      socket.broadcast.emit("metadata", data);
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+  //! Call on machine detail data...................................................................................
+  app.post("/api/machine/details/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = eval(req.body);
+    try {
+      socket.broadcast.emit("md", data, id);
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+  //! Call on machine overview data.................................................................................
+  app.post("/api/machine/overview/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = eval(req.body);
+    try {
+      socket.broadcast.emit("mo", data, id);
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+  //! Call on machine overview bulk data.............................................................................
+  app.post("/api/machine/bulk/overview", async (req, res) => {
+    const data = eval(req.body);
+    try {
+      socket.broadcast.emit("mob", data);
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+
+  //! Call on machine detail bulk data................................................................................
+  app.post("/api/machine/details/:id", async (req, res) => {
+    const data = eval(req.body);
+    try {
+      socket.broadcast.emit("mdb", data);
+      res.send(data);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
+});
+
+//! Listening to server............................................................................................
+server.listen(8000, () => {
+  console.log(`Server listening on port 8000`);
+});
+
 // class RealtimeService {
 //   constructor() {
 //     this.io = new Server(server, {
@@ -81,72 +154,6 @@ let data = [];
 
 // const realTimeService = new RealtimeService();
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("Connected to client");
-  //! Call on machine metadata.......................................................................................
-  app.post("/api/machine/metadata", async (req, res) => {
-    data = eval(req.body);
-    try {
-      socket.broadcast.emit("metadata", data);
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  //! Call on machine detail data...................................................................................
-  app.post("/api/machine/details/:id", async (req, res) => {
-    const { id } = req.params;
-    const data = eval(req.body);
-    try {
-      socket.broadcast.emit("md", data, id);
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  //! Call on machine overview data.................................................................................
-  app.post("/api/machine/overview/:id", async (req, res) => {
-    const { id } = req.params;
-    const data = eval(req.body);
-    try {
-      socket.broadcast.emit("mo", data, id);
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  //! Call on machine overview bulk data.............................................................................
-  app.post("/api/machine/bulk/overview", async (req, res) => {
-    const data = eval(req.body);
-    try {
-      socket.broadcast.emit("mob", data);
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-
-  //! Call on machine detail bulk data................................................................................
-  app.post("/api/machine/details/:id", async (req, res) => {
-    const data = eval(req.body);
-    try {
-      socket.broadcast.emit("mdb", data);
-      res.send(data);
-    } catch (error) {
-      res.status(500).send(error.message);
-    }
-  });
-});
 
 // //! Call on machine metadata.......................................................................................
 // app.post("/api/machine/metadata", async (req, res) => {
@@ -205,7 +212,5 @@ io.on("connection", (socket) => {
 //   }
 // });
 
-//! Listening to server............................................................................................
-server.listen(8000, () => {
-  console.log(`Server listening on port 8000`);
-});
+
+
